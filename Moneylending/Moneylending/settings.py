1687e84aa772c82e2c5a7ef546fe542f909533a9
Manish Chaudhary <chaudhary94rc@gmail.com>
2019-11-27 22:03:06 +0530

    add user logic

--- Moneylending/Moneylending/settings.py
+++ Moneylending/Moneylending/settings.py
@@ -120,6 +120,11 @@ USE_L10N = True
 
 USE_TZ = True
 
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'chaudhary.manish@thinksys.com'
+EMAIL_HOST_PASSWORD = ''
+EMAIL_PORT = 587
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
--- Moneylending/moneymanagement/views.py
+++ Moneylending/moneymanagement/views.py
@@ -1,5 +1,8 @@
+from django.core.mail import send_mail
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib.auth.models import User,auth
+from django.conf import settings
 
 # Create your views here.
 
@@ -8,10 +11,40 @@ def home(request):
 
 
 def register(request):
-    return render(request,'register.html')
-
+    if request.method == 'POST':
+        firstname= request.POST['firstname']
+        lastname= request.POST['lastname']
+        country= request.POST['country']
+        dateofbirth= request.POST['dateofbirth']
+        addressline1= request.POST['addressline1']
+        addressline2= request.POST['addressline2']
+        company= request.POST['company']
+        city= request.POST['city']
+        postalcode= request.POST['postalcode']
+        email= request.POST['email']
+        mobileno= request.POST['mobileno']
+        ssnno= request.POST['ssnno']
+        phoneno= request.POST['phoneno']
+        
+        if User.objects.filter(username=email).exists():
+            print('email exits')
+            send_mail(
+            'Subject here',
+            'Here is the message.',
+            'chaudhary94rc@gmail.com',
+            'chaudhary94rc@gmail.com',
+            fail_silently=False,)
+        else:
+            print(firstname)
+            return render(request,'home.html',{'module':'register'})
+    else:
+         return render(request,'home.html',{'module':'register'})
+     
 def login(request):
-    return render(request,'login.html')
+    if request.method == 'POST':
+        send_mail('Subject here', 'Here is the message.', settings.EMAIL_HOST_USER,
+         ['to@example.com'], fail_silently=False)
+    return render(request,'home.html',{'module':'login'})
 
 def contact(request):
-    return render(request,'contact.html')
+    return render(request,'home.html',{'module':'contact'})
